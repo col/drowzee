e61f1dd1022545e8106fb08211949350fb292e12
Colin Harris <colin@jiva.ag>
2025-02-25 22:16:24 +1100

    Enable debug logging in prod
    Not recommended but I need to see what's going on for now

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ import Config
 config :drowzee, DrowzeeWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 config :drowzee, Drowzee.K8sConn, method: :service_account
 
