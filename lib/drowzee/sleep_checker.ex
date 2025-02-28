c203c6af08219c1c4528ee018ec0861190f1ea2f
Colin Harris <colin@jiva.ag>
2025-02-28 12:57:00 +1100

    Fix naptime calculations

--- lib/drowzee/sleep_checker.ex
+++ lib/drowzee/sleep_checker.ex
@@ -5,33 +5,33 @@ defmodule Drowzee.SleepChecker do
     now = DateTime.now!(timezone)
     today_date = DateTime.to_date(now)
 
-    {:ok, sleep_datetime} = parse_time(sleep_time, today_date, timezone)
-    {:ok, wake_datetime} = parse_time(wake_time, today_date, timezone)
+    with {:ok, sleep_datetime} <- parse_time(sleep_time, today_date, timezone),
+         {:ok, wake_datetime} <- parse_time(wake_time, today_date, timezone) do
+      Logger.debug("Sleep time: #{inspect(sleep_datetime)}, Wake time: #{inspect(wake_datetime)}, Now: #{inspect(now)}")
 
-    wake_datetime =
-      if DateTime.compare(sleep_datetime, wake_datetime) == :gt do
-        DateTime.add(wake_datetime, 86400, :second) # Move wake time to the next day
-      else
-        wake_datetime
+      case DateTime.compare(sleep_datetime, wake_datetime) do
+        :gt ->
+          DateTime.compare(now, sleep_datetime) in [:eq, :gt] or DateTime.compare(now, wake_datetime) == :lt
+        :lt ->
+          DateTime.compare(now, sleep_datetime) in [:eq, :gt] and DateTime.compare(now, wake_datetime) == :lt
+        :eq ->
+          Logger.warning("Sleep time and wake time cannot be the same!")
+          false
       end
-
-    Logger.debug("Sleep time: #{inspect(sleep_datetime)}, Wake time: #{inspect(wake_datetime)}, Now: #{inspect(now)}")
-    DateTime.compare(now, sleep_datetime) in [:eq, :gt] and DateTime.compare(now, wake_datetime) == :lt
-  end
-
-  def parse_time(time_str, timezone) do
-    today_date = Date.utc_today()
-    parse_time(time_str, today_date, timezone)
+    else
+      {:error, error} ->
+        Logger.error("Failed to parse time: #{inspect(error)}")
+        false
+    end
   end
 
   defp parse_time(time_str, date, timezone) do
     case Timex.parse(time_str, "%I:%M%p", :strftime) do
       {:ok, time} ->
-        datetime =
-          DateTime.new!(date, Time.new!(time.hour, time.minute, 0), timezone)
+        datetime = DateTime.new!(date, Time.new!(time.hour, time.minute, 0), timezone)
         {:ok, datetime}
-
-      error -> error
+      {:error, error} ->
+        {:error, error}
     end
   end
 end
