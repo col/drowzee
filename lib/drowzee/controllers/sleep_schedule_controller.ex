29f1da7ffccbe4a00dc594731f0b9f314513a974
Anton Bastin <anton.bastin@chirpwireless.io>
2025-05-08 10:24:12 +0200

    fix: Update enable/disable field

--- lib/drowzee/controllers/sleep_schedule_controller.ex
+++ lib/drowzee/controllers/sleep_schedule_controller.ex
@@ -11,7 +11,7 @@ defmodule Drowzee.Controller.SleepScheduleController do
   step Bonny.Pluggable.SkipObservedGenerations
   step :handle_event
 
-  def handle_event(%Bonny.Axn{resource: %{"spec" => %{"enabled" => false}}} = axn, _opts) do
+  def handle_event(%Bonny.Axn{resource: %{"spec" => spec}} = axn, _opts) when Map.get(spec, "enabled", true) == false do
     Logger.info("Schedule #{axn.resource["metadata"]["name"]} is disabled. Skipping.")
     axn
   end
